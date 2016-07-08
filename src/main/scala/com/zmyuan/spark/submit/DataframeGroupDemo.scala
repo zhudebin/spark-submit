8aa76c942d2704ea8875f5b98cf07ea7882bc597
zhudebin <zdb_hn@163.com>
2016-07-08 18:16:35 +0800

    dataframe group

---
+++ src/main/scala/com/zmyuan/spark/submit/DataframeGroupDemo.scala
@@ -0,0 +1,47 @@
+package com.zmyuan.spark.submit
+
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Row, SparkSession}
+
+import scala.math._
+
+/**
+  * Created by zhudebin on 16/7/8.
+  */
+object DataframeGroupDemo {
+
+  val loggerName = this.getClass.getName
+  lazy val logger = Logger.getLogger(loggerName)
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setMaster("local[4]").setAppName("test")
+
+    val spark = SparkSession
+    .builder
+      .config(conf)
+    .appName("Spark group")
+    .getOrCreate()
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+
+    val rdd:RDD[Row] = spark.sparkContext.parallelize(1 until 1000).map{
+      i => Row(i, s"name$i", i % 10)
+    }
+
+    val schema = StructType(Seq(StructField("id", IntegerType, false),
+      StructField("name", StringType, false), StructField("age", IntegerType, false)))
+
+    val df = spark.createDataFrame(rdd, schema)
+
+    df.groupBy("age").agg($"age", max("name"), sum("id")).collect().foreach(println _)
+
+
+    spark.stop()
+
+  }
+
+}
